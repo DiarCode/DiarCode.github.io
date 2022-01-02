ee80e86d4d9ebd638c4b61ce46270f57d595245e
DiarCode <diar_beg64@mail.ru>
2022-01-02 21:18:21 +0600

    burger

--- app/main.js
+++ app/main.js
@@ -1,1 +1,15 @@
+const navbar = document.querySelector(".navbar__nav");
+const navClose = document.querySelector(".navbar__close");
+const navOpen = document.querySelector(".navbar__open");
 
+navOpen.addEventListener("click", show);
+navClose.addEventListener("click", close);
+
+function show() {
+    navbar.style.display = "flex";
+    navbar.style.top = "0";
+}
+
+function close() {
+    navbar.style.top = "-100%"
+}
